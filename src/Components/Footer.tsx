0ec52a0b1c356cc723690fef7658766e1034f08c
WTL04 <williamluu04@gmail.com>
2026-02-26 16:59:39 -0800

    Updates

--- src/Components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import "./Footer.css"
-export default function Footer() {
-  return (
-    <footer className="rectangle">
-      <p>© 2025 William Luu. All Rights Reserved</p>
-    </footer>
-  );
-}
